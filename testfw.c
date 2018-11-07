30d96822786194e8784366f349915e9e8d76e17c
ljolliet <ljolliet@etu.u-bordeaux.fr>
2018-11-07 19:10:26 +0100

    Framework init

--- testfw.c
+++ testfw.c
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE
+#define TEST_SIZE 20
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -6,30 +7,49 @@
 
 /* ********** STRUCTURES ********** */
 
-struct testfw_t
+typedef struct testfw_t // NOT SUR OF THE WAY TO DECLARE IT
 {
-    /* ... */
-};
+    char *program;
+    int timeout;
+    char *logfile;
+    char *cmd;
+    bool silent;
+    bool verbose;
+    struct test_t * tests[TEST_SIZE];
+}testfw_t;
 
 /* ********** FRAMEWORK ********** */
 
 struct testfw_t *testfw_init(char *program, int timeout, char *logfile, char *cmd, bool silent, bool verbose)
 {
-    return NULL;
+    //TO DO ? : ALLOC/MALLOC/REALLOC I DON'T KNOW
+    testfw_t t = { .program = program, .timeout = timeout, .logfile = logfile, .cmd = cmd, .silent = silent, .verbose = verbose };
+    for(int i = 0; i< TEST_SIZE; i++)
+        t.tests[i] = NULL;
+    return &t;
 }
 
 void testfw_free(struct testfw_t *fw)
 {
+    //TODO : FREE THE STRUCT CONTENT AND THE STRUCTURE ITSELF ?
 }
 
 int testfw_length(struct testfw_t *fw)
 {
-    return 0;
+    // SHOULD BE ENOUGH
+    int length = 0;
+    for(int i = 0; i<TEST_SIZE; i++)
+       if(fw->tests[i] != NULL)
+            length++;
+    return length;
 }
 
 struct test_t *testfw_get(struct testfw_t *fw, int k)
 {
-    return NULL;
+    // SHOULD BE ENOUGH
+    if(k>=TEST_SIZE)    // to avoid a crash
+        return NULL;
+    return fw->tests[k];
 }
 
 /* ********** REGISTER TEST ********** */
